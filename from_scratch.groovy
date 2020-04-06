8748363c8f997ef5c97505978bd0d5719dae03f2
yunus nalbant <yunusenalbant@gmail.com>
2020-04-06 00:54:09 -0500

    added environments

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -22,18 +22,18 @@ properties([
     }
    stage("Install Prerequisites"){
 		sh """
-		ssh centos@dev1.ayyildizrug.com                 sudo yum install httpd -y
+		ssh centos@${ENVIR}                 sudo yum install httpd -y
 	
 		"""
     }
 
 	stage("Copy Artifacts"){
 		sh """
-		scp -r *  centos@dev1.ayyildizrug.com:/tmp
-		ssh centos@dev1.ayyildizrug.com                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@dev1.ayyildizrug.com                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@dev1.ayyildizrug.com				   sudo chown centos:centos /var/www/html/
-		ssh centos@dev1.ayyildizrug.com				   sudo chmod 777 /var/www/html/*
+		scp -r *  centos@${ENVIR}:/tmp
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@${ENVIR}				   sudo chown centos:centos /var/www/html/
+		ssh centos@${ENVIR}				   sudo chmod 777 /var/www/html/*
 		"""
 	}
 	stage("Restart  web server"){
