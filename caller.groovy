6477dee5ce30a9da24980f6c54906225d742535c
yunus nalbant <yunusenalbant@gmail.com>
2020-04-09 02:09:15 -0500

    added 2 more job on caller.groovy

--- caller.groovy
+++ caller.groovy
@@ -1,17 +1,24 @@
+  
 node {
 	stage("Stage1"){
 		echo "hello"
 }
 	stage("Stage2"){
 		echo "hello"
+        // Calls another job called "Timestamp"
+        build 'timestamp'
 }
 	stage("Stage3"){
 		echo "hello"
+        // Calls another job called "Script"
+        build "script"
 }
 	stage("Stage4"){
 		build 'Template2'
+        // Calls another job called "Script"
 }
 	stage("Stage5"){
 		echo "hello"
 	}
+    
 }
