e641eaddc63bac5b2c632f175ce688ade70b2b7b
yunus nalbant <yunusenalbant@gmail.com>
2020-04-09 22:06:49 -0500

    added label to last-build.groovy

--- last-build.groovy
+++ last-build.groovy
@@ -1,4 +1,4 @@
-node {
+node ('linux'){
     properties([
         // Keeps only last 5
         buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
