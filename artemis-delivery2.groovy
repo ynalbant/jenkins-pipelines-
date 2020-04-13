63417b253e8e99bf723e2f14c83184f70b08a1e9
yunus nalbant <yunusenalbant@gmail.com>
2020-04-13 02:48:19 -0500

    +

--- artemis-delivery2.groovy
+++ artemis-delivery2.groovy
@@ -30,7 +30,7 @@ properties(
                         timestamps {
                                 ws{
                                         sh '''
-                                                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 108879318566.dkr.ecr.us-east-1.amazonaws.com/artemis
+						aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 108879318566.dkr.ecr.us-east-1.amazonaws.com/artemis
                                                 '''
                                 }
                         }
